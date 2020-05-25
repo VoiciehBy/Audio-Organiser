c848090616f22608aa95b99fce7472de072f6fbb
RafKur <Rafal.kurzdym@gmail.com>
2020-05-25 16:56:39 +0200

    Add files via upload

---
+++ Audio-Organiser/Audio_Organiser/SongExt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audio_Organiser
+{
+    partial class Song
+    {
+        public override string ToString()
+        {
+
+            var f = TagLib.File.Create(Path);
+
+            return f.Tag.FirstAlbumArtist + " - " + f.Tag.Title + " from " + f.Tag.Album;
+        }
+    }
+}
