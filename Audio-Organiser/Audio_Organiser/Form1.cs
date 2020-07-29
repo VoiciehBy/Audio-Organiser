80a3e4e156cf884afeb3d282625cc8d4a9520bdb
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2020-07-29 15:43:34 +0200

    Preparation to translation fix

--- Audio-Organiser/Audio_Organiser/Form1.cs
+++ Audio-Organiser/Audio_Organiser/Form1.cs
@@ -38,24 +38,24 @@ namespace Audio_Organiser
         DatabaseMusicDataContext DatabaseDC = new DatabaseMusicDataContext();
 		
         //translation   		
-		string badFormat = "Zły format pliku. Tylko mp3 i wav.";
-		string fileIsOpenOr = "Plik jest otwarty w innym programie, bądź odtwarzany w playerze.";
-        string fileWithGivenPath = "Plik o ścieżce zapisanej w bazie nie istnieje ";
-        string fileWithGivenName = "Plik o podanej nazwie docelowej już istnieje.";
-        string fWGPAWGN = fileWithGivenPath + "oraz plik o podanej nazwie docelowej już istnieje.";
-        string warning = "Ostrzeżenie";      
-		string areYouSureDel = "Czy na pewno chcesz usunąć ten wpis z bazy danych?";
-        string areYouSureDel1 = "Czy na pewno chcesz usunąć ten plik z dysku?";
-		string question = "Pytanie";
-        string doYouWant = "Czy chcesz usunąć ten wpis z bazy danych?";
-        string fileDontExist = "Plik nie istnieje.";
-		string wantDelItems = "Czy na pewno chcesz usunąć elementy widoczne na liście?";
-        string fileIsPlayed = "Plik jest aktualnie odtwarzany.";	
-		string info = "Informacja";
-		string showedEntryWithWrongPathes = "Wyświetlono wpisy z błędnymi ścieżkami.";	   
-		string cannotFindFile = "Nie można znaleźć pliku:\n";
-		string delFromListAndTryAgain = "\nUsuń go z listy odtwarzacza i spróbuj ponownie.";
-		string selectResolution = "Wybierz rozdzielczość:";
+		public static string badFormat = "Zły format pliku. Tylko mp3 i wav.";
+		public static string fileIsOpenOr = "Plik jest otwarty w innym programie, bądź odtwarzany w playerze.";
+        public static string fileWithGivenPath = "Plik o ścieżce zapisanej w bazie nie istnieje ";
+        public static string fileWithGivenName = "Plik o podanej nazwie docelowej już istnieje.";
+        public static string fWGPAWGN = fileWithGivenPath + "oraz plik o podanej nazwie docelowej już istnieje.";
+        public static string warning = "Ostrzeżenie";      
+		public static string areYouSureDel = "Czy na pewno chcesz usunąć ten wpis z bazy danych?";
+        public static string areYouSureDel1 = "Czy na pewno chcesz usunąć ten plik z dysku?";
+		public static string question = "Pytanie";
+        public static string doYouWant = "Czy chcesz usunąć ten wpis z bazy danych?";
+        public static string fileDontExist = "Plik nie istnieje.";
+		public static string wantDelItems = "Czy na pewno chcesz usunąć elementy widoczne na liście?";
+        public static string fileIsPlayed = "Plik jest aktualnie odtwarzany.";	
+		public static string info = "Informacja";
+		public static string showedEntryWithWrongPathes = "Wyświetlono wpisy z błędnymi ścieżkami.";	   
+		public static string cannotFindFile = "Nie można znaleźć pliku:\n";
+		public static string delFromListAndTryAgain = "\nUsuń go z listy odtwarzacza i spróbuj ponownie.";
+		public static string selectResolution = "Wybierz rozdzielczość:";
 
         //player
         private WaveOutEvent outputDevice;
