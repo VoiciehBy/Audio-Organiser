7fe4585ca74ca7b05860889e9e597de0d5947ebb
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2021-03-26 04:54:56 +0100

    Delete Kontrolka.cs.

--- Audio-Organiser/Audio_Organiser/DatabaseMusic.designer.cs
+++ Audio-Organiser/Audio_Organiser/DatabaseMusic.designer.cs
@@ -11,18 +11,12 @@
 
 namespace Audio_Organiser
 {
-	using System.Data.Linq;
-	using System.Data.Linq.Mapping;
-	using System.Data;
-	using System.Collections.Generic;
-	using System.Reflection;
-	using System.Linq;
-	using System.Linq.Expressions;
-	using System.ComponentModel;
-	using System;
-	
-	
-	[global::System.Data.Linq.Mapping.DatabaseAttribute(Name="muzyka")]
+    using System.Data.Linq.Mapping;
+    using System.ComponentModel;
+    using System;
+
+
+    [global::System.Data.Linq.Mapping.DatabaseAttribute(Name="muzyka")]
 	public partial class DatabaseMusicDataContext : System.Data.Linq.DataContext
 	{
 		
