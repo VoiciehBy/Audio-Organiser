1b92a84a382a614630859e82555254c288ee6e95
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2021-09-23 05:47:08 +0200

    Resolution fix.

--- Audio-Organiser/Audio_Organiser/Form2.cs
+++ Audio-Organiser/Audio_Organiser/Form2.cs
@@ -15,22 +15,22 @@ namespace Audio_Organiser
             MainWindow mW = new MainWindow();
             if (resolutionBox.SelectedItem == resolutionBox.Items[0])
             {
-                Width = 1280;
-                Height = 768;
+                mW.Width = 1280;
+                mW.Height = 768;
                 mW.moveButtonsEtc(0, 0);
                 mW.resizeColumns(0, 0, 272, 189, 164, 137, 59, 149);
             }
             else if (resolutionBox.SelectedItem == resolutionBox.Items[1])
             {
-                Width = 1024;
-                Height = 768;
+                mW.Width = 1024;
+                mW.Height = 768;
                 mW.moveButtonsEtc(-250, 0);
                 mW.resizeColumns(0, 0, 150, 135, 145, 140, 60, 90);
             }
             else if (resolutionBox.SelectedItem == resolutionBox.Items[2])
             {
-                Width = 800;
-                Height = 768;
+                mW.Width = 800;
+                mW.Height = 768;
                 mW.moveButtonsEtc(-350, 0, -503, 29);
                 mW.resizeColumns(0, 0, 110, 85, 100, 80, 50, 60);
             }
