83ceefba9fc51be353424262c756242ae4f14956
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2023-07-04 10:40:04 +0200

    Fixed missing yt-dlp binary file after build, deleted the "installer" reduntant files...

--- Audio_Organiser/Form3.cs
+++ Audio_Organiser/Form3.cs
@@ -14,13 +14,13 @@ namespace Audio_Organiser
 
         private void downloadButton_Click(object sender, EventArgs e)
         {
-            string projectRootPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\"));
+            string baseDirectory = Path.GetFullPath(AppDomain.CurrentDomain.BaseDirectory);
             string songNameOrUrl = linkTextBox.Text;
-            string output = projectRootPath + "\\songs\\" + "%(title)s.%(ext)s";
+            string output = baseDirectory + "\\songs\\" + "%(title)s.%(ext)s";
             string arguments = "-o \"" + output + "\" ytsearch:\"" + songNameOrUrl + "\" -x --audio-format mp3 --embed-thumbnail --add-metadata";
             Process downloading = new Process();
 
-            downloading.StartInfo.FileName = projectRootPath + "yt-dlp.exe";
+            downloading.StartInfo.FileName = baseDirectory + "yt-dlp.exe";
             downloading.StartInfo.UseShellExecute = false;
             downloading.StartInfo.CreateNoWindow = false;
             downloading.StartInfo.Arguments = arguments;
@@ -28,10 +28,10 @@ namespace Audio_Organiser
             downloading.WaitForExit();
             MainWindow mainWindow = FormManager.mW;
 
-            if (!(Directory.Exists(projectRootPath + "\\songs\\")))
-                Directory.CreateDirectory(projectRootPath + "\\songs\\");
+            if (!(Directory.Exists(baseDirectory + "\\songs\\")))
+                Directory.CreateDirectory(baseDirectory + "\\songs\\");
             
-            string[] filePaths = Directory.GetFiles(projectRootPath + "\\songs\\");
+            string[] filePaths = Directory.GetFiles(baseDirectory + "\\songs\\");
             foreach (string filePath in filePaths) mainWindow.addF(filePath);
             this.Close();
             mainWindow.Show();
